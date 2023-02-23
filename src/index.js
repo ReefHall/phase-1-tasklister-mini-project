039e21e5d30c45352359d9a7deec70f9d02c1928
Shareif Hall <shareifhall@yahoo.com>
2023-02-23 17:34:36 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form")
+  const input = document.getElementById("new-task-description")
+  const ul = document.getElementById("tasks")
+
+  const dropDown = document.getElementById("dropdown")
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const list = document.createElement("li")
+    ul.append(list);
+    list.innerText = input.value;
+    const button = document.createElement("button")
+    list.append(button)
+    button.innerText = "DELETE";
+    button.addEventListener("click", () => {
+      list.remove();
+      
+       
+    })
+    if(dropDown.value === "high") {
+      list.style.color = "red"
+
+    }
+    if(dropDown.value === "medium") {
+      list.style.color = "orange"
+    }
+    if(dropDown.value === "low") {
+      list.style.color = "blue"
+    }
+
+  })
+})
+
+
+ 
+ 
+  
+
+
+
+ 
+
